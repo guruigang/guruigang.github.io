2bb04e83342d5e2754b4dd34b507dd0b933b16d3
guruigang <r1gu@ucsd.edu>
2016-03-05 17:53:07 -0800

    change type of auto fill

--- js/createEvent.js
+++ js/createEvent.js
@@ -12,9 +12,7 @@ function initAutocomplete() {
    // Create the autocomplete object, restricting the search to geographical
    // location types.
     autocomplete = new google.maps.places.Autocomplete(
-     /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-      {types: ['all']});
-
+     /** @type {!HTMLInputElement} */(document.getElementById('autocomplete'));
         // When the user selects an address from the dropdown, populate the address
      
  }
